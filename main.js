848c38e423a7e6067116b198ca3d6dab07a59d9f
sumitm001 <sumit.m@reidnax.com>
2024-05-21 12:03:18 +0530

    ch03: end

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 import express from 'express'
+import { createElement } from 'react'
+import { renderToString } from 'react-dom/server'
+import App from './components/App.jsx'
 
 const PORT = 3000
 const app = express();
 
+// creating a react components
+// App div -> h1
+// const h1 = createElement('h1', null, 'Hello, from Server')
+// const App = createElement('div', null, h1)
+
 app.get('/', (req, res) => {
-	res.send('Hello, World!');
+	const appHTML = renderToString(createElement(App))
+	res.send(`
+		<!DOCTYPE>
+		<html lang="en">
+			<head>
+				<title>React SSR App</title>
+			</head>
+			<body>
+				${appHTML}
+			</body>
+		</html>
+	`);
 })
 
 app.listen(PORT, () => {
