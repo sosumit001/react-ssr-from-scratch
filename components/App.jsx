95cbd0928eaf1a6165981cbf4c66118af968ab7c
sumitm001 <sumit.m@reidnax.com>
2024-05-21 12:03:18 +0530

    s This is a combination of 2 commits.
    ch03: end

---
+++ components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const App = () => {
+	return (
+		<div>
+			<h1>
+				Hello From Server!!
+			</h1>
+		</div>
+	);
+}
+ 
+export default App;
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,14 @@
 const { default: commonjs } = require("@rollup/plugin-commonjs");
 const { default: nodeResolve } = require("@rollup/plugin-node-resolve");
+const { default: babel } = require('@rollup/plugin-babel');
 
+const babelOptions = {
+	presets: [
+		'@babel/preset-react',
+	],
+	babelHelpers: 'bundled',
+	exclude: 'node_modules/**'
+}
 module.exports =  {
 	input: 'main.js',
 	output: [
@@ -11,9 +19,12 @@ module.exports =  {
 	],
 	plugins: [
 		nodeResolve({
+			extensions: ['js', 'jsx'],
 			preferBuiltins: true
 		}),
-		commonjs()
+		babel(babelOptions),
+		commonjs(),
+		
 	],
 	external: ['express', 'react', 'react-dom']
 }
